c9f269d81fd831cb41493bb8066ce0904de3cbe6
David Robert <contact.davidrobert@gmail.com>
2021-07-15 14:07:14 +0200

    form type customisation

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,7 +4,13 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +19,21 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('createdAt')
-            ->add('isPublished')
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu'
+            ])
+            ->add('createdAt', DateType::class, [
+                'widget' => 'single_text',
+                'label' => "Date de création",
+                'data' => new \DateTime('NOW')
+            ])
+            ->add('isPublished', CheckboxType::class, [
+                'label' => 'Publié ?',
+                'data' => true
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
